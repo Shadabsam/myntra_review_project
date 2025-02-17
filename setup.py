c1538574c13128529cba9e94a1c2f11659fa89b5
shadabsam <shadabsam91751@gmail.com>
2025-02-18 01:03:51 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='shadab',
+    author_email='shadabsam91751@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
