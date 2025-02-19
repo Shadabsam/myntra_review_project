b5c1a2beb230203fe28b6381b5331ce3e3ef7b2f
shadabsam <shadabsam91751@gmail.com>
2025-02-19 17:06:18 +0530

    added few codes

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,4 @@
-MONGODB_URL_KEY: str = "mongodb+srv://shadab:<db_password>@cluster0.97tbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URL_KEY: str = "MONGO_DB_URL"
 MONGO_DATABASE_NAME: str = "myntra-reviews"
 
 
